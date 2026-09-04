d4468995e5a59600fbb6aafec958ab8f5d5b52f2
lilyco-42 <liuqingffffff@gmail.com>
2026-09-04 23:51:36 +0800

    fix: xmake package format (reference fmt package)

--- packages/webview-mini/xmake.lua
+++ packages/webview-mini/xmake.lua
@@ -1,16 +1,18 @@
 package("webview-mini")
     set_homepage("https://github.com/lilyco-42/webview-mini")
-    set_description("Minimal C/C++ WebView2 wrapper - single header, 200KB exe")
+    set_description("Minimal C/C++ WebView2 wrapper - single header, 200KB exe. Windows 23KB Android-style WebView.")
     set_license("MIT")
 
-    add_urls("https://github.com/lilyco-42/webview-mini.git")
-    add_versions("1.0.0", "main")  # 用 branch,或具体的 commit hash
+    set_urls("https://github.com/lilyco-42/webview-mini/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/lilyco-42/webview-mini.git")
+
+    add_versions("1.0.0", "main")
 
     add_syslinks("user32", "shell32", "ole32", "oleaut32", "shlwapi", "version")
     add_includedirs(".")
 
     on_install(function (package)
-        -- 单头文件,直接复制
+        -- Single header library, just copy the header
         os.cp("webview.h", package:installdir("include"))
     end)
 
